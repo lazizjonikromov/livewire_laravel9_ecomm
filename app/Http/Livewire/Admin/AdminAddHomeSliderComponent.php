cf7bd0e2758557061826c3a75a94be8ce8dcb846
Laziz <lazizjonweb@gmail.com>
2022-05-09 12:32:36 +0500

    work

--- app/Http/Livewire/Admin/AdminAddHomeSliderComponent.php
+++ app/Http/Livewire/Admin/AdminAddHomeSliderComponent.php
@@ -6,6 +6,18 @@ use Livewire\Component;
 
 class AdminAddHomeSliderComponent extends Component
 {
+    public $title;
+    public $subtitle;
+    public $price;
+    public $link;
+    public $image;
+    public $status;
+
+    public function mount()
+    {
+        
+    }
+
     public function render()
     {
         return view('livewire.admin.admin-add-home-slider-component')->layout('layouts.base');
--- resources/views/livewire/admin/admin-add-home-slider-component.blade.php
+++ resources/views/livewire/admin/admin-add-home-slider-component.blade.php
@@ -1,7 +1,78 @@
 <div>
     <div class="container" style="padding:30px 0">
         <div class="row">
-            
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Add New Slide
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{ route('admin.homeslider') }}" class="btn btn-success pull-right">
+                                    All Slides
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <form action="" class="form-horizontal">
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Title</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Title" class="form-control input-md">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Subtitle</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Subtitle" class="form-control input-md">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Price</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Price" class="form-control input-md">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Link</label>
+                                <div class="col-md-4">
+                                    <input type="text" placeholder="Link" class="form-control input-md">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Image</label>
+                                <div class="col-md-4">
+                                    <input type="file" class="input-file">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label">Status</label>
+                                <div class="col-md-4">
+                                    <select class="form-control">
+                                        <option value="0">Inactive</option>
+                                        <option value="1">Active</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="" class="col-md-4 control-label"></label>
+                                <div class="col-md-4">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
