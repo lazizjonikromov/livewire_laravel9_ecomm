e4a7856d1fb757b7fa46033b372386c04e6c91d5
Laziz <lazizjonweb@gmail.com>
2022-05-10 16:17:04 +0500

    work

--- app/Http/Livewire/Admin/AdminEditHomeSliderComponent.php
+++ app/Http/Livewire/Admin/AdminEditHomeSliderComponent.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\HomeSlider;
+use Carbon\Carbon;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -16,10 +17,11 @@ class AdminEditHomeSliderComponent extends Component
     public $image;
     public $status;
     public $newimage;
+    public $slider_id;
 
-    public function mount($slider_id)
+    public function mount($slide_id)
     {
-        $slider = HomeSlider::find($slider_id);
+        $slider = HomeSlider::find($slide_id);
         $this->title = $slider->title;
         $this->subtitle = $slider->subtitle;
         $this->price = $slider->price;
@@ -27,6 +29,24 @@ class AdminEditHomeSliderComponent extends Component
         $this->image = $slider->image;
         $this->status = $slider->status;
         $this->slider_id = $slider->id;
+    }
+
+    public function updateSlide()
+    {
+        $slider = HomeSlider::find($this->slider_id);
+        $slider->title = $this->title;
+        $slider->subtitle = $this->subtitle;
+        $slider->price = $this->price;
+        $slider->link = $this->link;
+        if($this->newimage)
+        {
+            $imagename = Carbon::now()->timestamp. '.' . $this->newimage->extension();
+            $this->newimage->storeAs('sliders', $imagename);
+            $slider->image = $imagename;
+        }
+        $slider->status = $this->status;
+        $slider->save();
+        session()->flash('message', 'Slide has been updated successfully!');
     }
 
     public function render()
