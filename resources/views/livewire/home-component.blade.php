a8129870b560b4046469ad657505b554fa8bb611
Laziz <lazizjonweb@gmail.com>
2022-05-12 15:24:09 +0500

    work

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -282,51 +282,47 @@
                 <div class="wrap-product-tab tab-style-1">
                     <div class="tab-control">
                         @foreach ($categories as $key => $category)
-                            <a href="#category_{{ $category_id }}"
-                                class="tab-control-item {{ $key == 0 ? 'active' : '' }}">Smartphone</a>
+                            <a href="#category_{{ $category->id }}"
+                                class="tab-control-item {{ $key == 0 ? 'active' : '' }}">{{ $category->name }}
+                            </a>
                         @endforeach
                     </div>
                     <div class="tab-contents">
+                        @foreach ($categories as $key=>$category)
+                            <div class="tab-content-item {{ $key == 0 ? 'active' : '' }}" id="category_{{ $category->id }}">
+                                <div class="wrap-products slide-carousel owl-carousel style-nav-1 equal-container"
+                                    data-items="5" data-loop="false" data-nav="true" data-dots="false"
+                                    data-responsive='{"0":{"items":"1"},"480":{"items":"2"},"768":{"items":"3"},"992":{"items":"4"},"1200":{"items":"5"}}'>
 
-                        <div class="tab-content-item active" id="fashion_1a">
-                            <div class="wrap-products slide-carousel owl-carousel style-nav-1 equal-container"
-                                data-items="5" data-loop="false" data-nav="true" data-dots="false"
-                                data-responsive='{"0":{"items":"1"},"480":{"items":"2"},"768":{"items":"3"},"992":{"items":"4"},"1200":{"items":"5"}}'>
+                                        @php
+                                            $c_products = DB::table('products')
+                                                ->where('category_id', $category->id)
+                                                ->get()
+                                                ->take($no_of_products);
+                                        @endphp
 
-                                @foreach ($categories as $key => $category)
-                                    @php
-                                        $c_products = DB::table('products')
-                                            ->where('category_id', $category_id)
-                                            ->get()
-                                            ->take($no_of_products);
-                                    @endphp
-
-                                    @foreach ($c_products as $c_product)
-                                        <div class="product product-style-2 equal-elem ">
-                                            <div class="product-thumnail">
-                                                <a href="{{ route('product.details', ['slug'=>$c_product->slug]) }}" title="{{ $c_product->name }}">
-                                                    <figure>
-                                                        <img src="{{ asset('assets/images/products') }}/{{ $c_product->image }}"
-                                                            width="800" height="800" alt="{{ $c_product->name }}">
-                                                    </figure>
-                                                </a>
-                                            </div>
-                                            <div class="product-info">
-                                                <a href="#"
-                                                    class="product-name"><span>{{ $c_product->name }}</span></a>
-                                                <div class="wrap-price"><span
-                                                        class="product-price">${{ $c_product->regular_price }}</span>
+                                        @foreach ($c_products as $c_product)
+                                            <div class="product product-style-2 equal-elem ">
+                                                <div class="product-thumnail">
+                                                    <a href="{{ route('product.details', ['slug'=>$c_product->slug]) }}" title="{{ $c_product->name }}">
+                                                        <figure>
+                                                            <img src="{{ asset('assets/images/products') }}/{{ $c_product->image }}"
+                                                                width="800" height="800" alt="{{ $c_product->name }}">
+                                                        </figure>
+                                                    </a>
+                                                </div>
+                                                <div class="product-info">
+                                                    <a href="{{ route('product.details', ['slug'=>$c_product->slug]) }}"
+                                                        class="product-name"><span>{{ $c_product->name }}</span></a>
+                                                    <div class="wrap-price"><span
+                                                            class="product-price">${{ $c_product->regular_price }}</span>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    @endforeach
-                                @endforeach
-
-
-
+                                        @endforeach
+                                </div>
                             </div>
-                        </div>
-
+                        @endforeach
                     </div>
                 </div>
             </div>
