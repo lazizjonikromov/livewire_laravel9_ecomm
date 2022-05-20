50e97cbc1e44c3040b23be5829331317b1f8ac20
Laziz <lazizjonweb@gmail.com>
2022-05-20 11:20:42 +0500

    work

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -60,7 +60,7 @@
                                             <a href="{{ route('admin.editproduct',['product_slug'=>$product->slug]) }}">
                                                 <i class="fa fa-edit fa-2x text-info"></i>
                                             </a>
-                                            <a href="#" wire:click.prevent="deleteProduct({{$product->id}})" style="margin-left: 10px">
+                                            <a href="#" onclick="confirm('Are you sure, You want to delete this product?') || event.stopImmediatePropagation()" wire:click.prevent="deleteProduct({{$product->id}})" style="margin-left: 10px">
                                                 <i class="fa fa-times fa-2x text-danger"></i>
                                             </a>
                                         </td>
