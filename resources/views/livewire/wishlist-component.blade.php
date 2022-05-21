758fa6ce9132c821b6665882cc0d9ac8377d8332
Laziz <lazizjonweb@gmail.com>
2022-05-21 16:15:18 +0500

    work

--- resources/views/livewire/wishlist-component.blade.php
+++ resources/views/livewire/wishlist-component.blade.php
@@ -12,44 +12,40 @@
         <div class="row">
             @if (Cart::instance('wishlist')->content()->count() > 0)
 
-            @endif
-            <ul class="product-list grid-products equal-container">
-                @foreach ($products as $product)
-                    <li class="col-lg-4 col-md-6 col-sm-6 col-xs-6 ">
-                        <div class="product product-style-3 equal-elem ">
-                            <div class="product-thumnail">
-                                <a href="{{ route('product.details', ['slug' => $product->slug]) }}"
-                                    title="{{ $product->name }}">
-                                    <figure><img
-                                            src="{{ asset('assets/images/products') }}/{{ $product->image }}"
-                                            alt="{{ $product->name }}"></figure>
-                                </a>
-                            </div>
-                            <div class="product-info">
-                                <a href="{{ route('product.details', ['slug' => $product->slug]) }}"
-                                    class="product-name"><span>{{ $product->name }}</span></a>
-                                <div class="wrap-price"><span
-                                        class="product-price">${{ $product->regular_price }}</span></div>
-                                <a href="#" class="btn add-to-cart"
-                                    wire:click.prevent="store({{ $product->id }} , '{{ $product->name }}' ,{{ $product->regular_price }})">Add
-                                    To Cart</a>
-                                <div class="product-wish">
-                                    @if($witems->contains($product->id))
-                                        <a href="#" wire:click.prevent="removeFromWishlist({{ $product->id }})">
+                <ul class="product-list grid-products equal-container">
+                    @foreach (Cart::instance('wishlist')->content() as $item)
+                        <li class="col-lg-4 col-md-6 col-sm-6 col-xs-6 ">
+                            <div class="product product-style-3 equal-elem ">
+                                <div class="product-thumnail">
+                                    <a href="{{ route('product.details', ['slug' => $item->model->slug]) }}"
+                                        title="{{ $item->model->name }}">
+                                        <figure><img
+                                                src="{{ asset('assets/images/products') }}/{{ $item->model->image }}"
+                                                alt="{{ $item->model->name }}"></figure>
+                                    </a>
+                                </div>
+                                <div class="product-info">
+                                    <a href="{{ route('product.details', ['slug' => $item->model->slug]) }}"
+                                        class="product-name"><span>{{ $item->model->name }}</span></a>
+                                    <div class="wrap-price"><span
+                                            class="product-price">${{ $item->model->regular_price }}</span></div>
+                                    <a href="#" class="btn add-to-cart"
+                                        wire:click.prevent="store({{ $item->model->id }} , '{{ $item->model->name }}' ,{{ $item->model->regular_price }})">Add
+                                        To Cart</a>
+                                    <div class="product-wish">
+                                        <a href="#" wire:click.prevent="removeFromWishlist({{ $item->model->id }})">
                                             <i class="fa fa-heart fill-heart"></i>
                                         </a>
-                                    @else
-                                        <a href="#" wire:click.prevent="addToWishlist({{ $product->id }} , '{{ $product->name }}' ,{{ $product->regular_price }})">
-                                            <i class="fa fa-heart"></i>
-                                        </a>
-                                    @endif
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </li>
-                @endforeach
+                        </li>
+                    @endforeach
+                </ul>
 
-            </ul>
+            @else
+                <h1>No item in wishlist</h1>
+            @endif
 
         </div>
 
