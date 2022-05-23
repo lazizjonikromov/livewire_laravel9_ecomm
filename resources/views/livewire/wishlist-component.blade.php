484cb9125474f7254bc67a2c48627bf2ce02d3b1
Laziz <lazizjonweb@gmail.com>
2022-05-23 15:13:12 +0500

    work

--- resources/views/livewire/wishlist-component.blade.php
+++ resources/views/livewire/wishlist-component.blade.php
@@ -75,8 +75,7 @@
                                     <div class="wrap-price"><span
                                             class="product-price">${{ $item->model->regular_price }}</span></div>
                                     <a href="#" class="btn add-to-cart"
-                                        wire:click.prevent="store({{ $item->model->id }} , '{{ $item->model->name }}' ,{{ $item->model->regular_price }})">Move
-                                        To Cart</a>
+                                        wire:click.prevent="moveProductFromWishlistToCart('{{ $item->rowId }}')">Move To Cart</a>
                                     <div class="product-wish">
                                         <a href="#" wire:click.prevent="removeFromWishlist({{ $item->model->id }})">
                                             <i class="fa fa-heart fill-heart"></i>
