b5257116369d671b05423e1493f468206dbd3d81
Laziz <lazizjonweb@gmail.com>
2022-05-26 16:47:49 +0500

    work

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $table="transactions";
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
 }
