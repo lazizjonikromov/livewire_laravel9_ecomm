b72b2eca53c2095f60cd7de4696918a93a897f9f
Laziz <lazizjonweb@gmail.com>
2022-05-26 15:46:06 +0500

    work

--- database/migrations/2022_05_26_103715_create_orders_table.php
+++ database/migrations/2022_05_26_103715_create_orders_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->decimal('subtotal');
+            $table->decimal('discount')->default(0);
+            $table->decimal('tax');
+            $table->decimal('total');
+            $table->string('firstname');
+            $table->string('lastname');
+
             $table->timestamps();
         });
     }
