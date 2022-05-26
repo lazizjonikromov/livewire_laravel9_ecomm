c8fbba9515275578e06b2f137d7dec5ba0ce8f89
Laziz <lazizjonweb@gmail.com>
2022-05-26 16:25:45 +0500

    work

--- database/migrations/2022_05_26_103850_create_order_items_table.php
+++ database/migrations/2022_05_26_103850_create_order_items_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('product_id')->unsigned();
+            $table->bigInteger('order_id')->unsigned();
+            $table->decimal('price');
+            $table->integer('quantity');
             $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
         });
     }
 
