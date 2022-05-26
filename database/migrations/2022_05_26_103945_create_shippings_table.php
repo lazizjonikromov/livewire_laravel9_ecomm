a301ab3ad2635d6165a6b0d06680908edfecad56
Laziz <lazizjonweb@gmail.com>
2022-05-26 16:29:19 +0500

    work

--- database/migrations/2022_05_26_103945_create_shippings_table.php
+++ database/migrations/2022_05_26_103945_create_shippings_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('shippings', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('order_id')->unsigned();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('mobile');
+            $table->string('email');
+            $table->string('line1');
+            $table->string('line2')->nullable();
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->string('zipcode');
             $table->timestamps();
         });
     }
