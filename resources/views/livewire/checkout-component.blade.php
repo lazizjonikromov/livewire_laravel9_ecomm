af17526a0eb3dd9525ca51832463533a8d73e313
Laziz <lazizjonweb@gmail.com>
2022-05-27 17:57:53 +0500

    work

--- resources/views/livewire/checkout-component.blade.php
+++ resources/views/livewire/checkout-component.blade.php
@@ -14,35 +14,35 @@
 					<form action="#" method="get" name="frm-billing">
 						<p class="row-in-form">
 							<label for="fname">first name<span>*</span></label>
-							<input id="fname" type="text" name="fname" value="" placeholder="Your name">
+							<input type="text" name="fname" value="" placeholder="Your name">
 						</p>
 						<p class="row-in-form">
 							<label for="lname">last name<span>*</span></label>
-							<input id="lname" type="text" name="lname" value="" placeholder="Your last name">
+							<input type="text" name="lname" value="" placeholder="Your last name">
 						</p>
 						<p class="row-in-form">
 							<label for="email">Email Addreess:</label>
-							<input id="email" type="email" name="email" value="" placeholder="Type your email">
+							<input type="email" name="email" value="" placeholder="Type your email">
 						</p>
 						<p class="row-in-form">
 							<label for="phone">Phone number<span>*</span></label>
-							<input id="phone" type="number" name="phone" value="" placeholder="10 digits format">
+							<input type="number" name="phone" value="" placeholder="10 digits format">
 						</p>
 						<p class="row-in-form">
 							<label for="add">Address:</label>
-							<input id="add" type="text" name="add" value="" placeholder="Street at apartment number">
+							<input type="text" name="add" value="" placeholder="Street at apartment number">
 						</p>
 						<p class="row-in-form">
 							<label for="country">Country<span>*</span></label>
-							<input id="country" type="text" name="country" value="" placeholder="United States">
+							<input type="text" name="country" value="" placeholder="United States">
 						</p>
 						<p class="row-in-form">
 							<label for="zip-code">Postcode / ZIP:</label>
-							<input id="zip-code" type="number" name="zip-code" value="" placeholder="Your postal code">
+							<input type="number" name="zip-code" value="" placeholder="Your postal code">
 						</p>
 						<p class="row-in-form">
 							<label for="city">Town / City<span>*</span></label>
-							<input id="city" type="text" name="city" value="" placeholder="City name">
+							<input type="text" name="city" value="" placeholder="City name">
 						</p>
 						<p class="row-in-form fill-wife">
 							<label class="checkbox-field">
