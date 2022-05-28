dc8074a8c9962d1e1b1102d95a05aa4d2c031d84
Laziz <lazizjonweb@gmail.com>
2022-05-28 11:30:30 +0500

    work

--- app/Http/Livewire/CheckoutComponent.php
+++ app/Http/Livewire/CheckoutComponent.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class CheckoutComponent extends Component
@@ -59,6 +61,26 @@ class CheckoutComponent extends Component
             'country' => 'required',
             'zipcode' => 'required'
         ]);
+
+        $order = new Order();
+        $order->user_id = Auth::user()->id;
+        $order->subtotal = session()->get('checkout')['subtotal'];
+        $order->discount = session()->get('checkout')['discount'];
+        $order->tax = session()->get('checkout')['tax'];
+        $order->total = session()->get('checkout')['total'];
+        $order->firstname = $this->firstname;
+        $order->lastname = $this->lastname;
+        $order->email = $this->email;
+        $order->mobile = $this->mobile;
+        $order->line1 = $this->line1;
+        $order->line2 = $this->line2;
+        $order->city = $this->city;
+        $order->province = $this->province;
+        $order->country = $this->country;
+        $order->zipcode = $this->zipcode;
+        $order->status = 'ordered';
+        $order->is_shipping_different = $this->ship_to_different ? ;
+
     }
 
     public function render()
