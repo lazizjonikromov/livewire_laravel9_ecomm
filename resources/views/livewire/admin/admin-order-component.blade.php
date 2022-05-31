5906bfcbb7dd77215e65fd42ad04fc53b6010743
Laziz <lazizjonweb@gmail.com>
2022-05-31 16:51:20 +0500

    work

--- resources/views/livewire/admin/admin-order-component.blade.php
+++ resources/views/livewire/admin/admin-order-component.blade.php
@@ -11,9 +11,37 @@
                             <thead>
                                 <tr>
                                     <th>OrderId</th>
-                                    <th>OrderId</th>
+                                    <th>Subtotal</th>
+                                    <th>Discount</th>
+                                    <th>Tax</th>
+                                    <th>Total</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Mobile</th>
+                                    <th>Email</th>
+                                    <th>Zipcode</th>
+                                    <th>Status</th>
+                                    <th>Order Date</th>
                                 </tr>
                             </thead>
+                            <tbody>
+                                @foreach ($orders as $order)
+                                    <tr>
+                                        <td>{{ $order->id }}</td>
+                                        <td>{{ $order->subtotal }}</td>
+                                        <td>{{ $order->discount }}</td>
+                                        <td>{{ $order->tax }}</td>
+                                        <td>{{ $order->total }}</td>
+                                        <td>{{ $order->firstname }}</td>
+                                        <td>{{ $order->lastname }}</td>
+                                        <td>{{ $order->mobile }}</td>
+                                        <td>{{ $order->email }}</td>
+                                        <td>{{ $order->zipcode }}</td>
+                                        <td>{{ $order->status }}</td>
+                                        <td>{{ $order->created_at }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
                     </div>
                 </div>
