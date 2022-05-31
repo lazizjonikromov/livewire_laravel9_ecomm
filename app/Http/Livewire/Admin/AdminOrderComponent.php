1e832697c14f049458c6340afd4792c2eafa81a9
Laziz <lazizjonweb@gmail.com>
2022-05-31 16:45:19 +0500

    work

--- app/Http/Livewire/Admin/AdminOrderComponent.php
+++ app/Http/Livewire/Admin/AdminOrderComponent.php
@@ -4,9 +4,12 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Order;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AdminOrderComponent extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         $orders = Order::orderBy('created_at','DESC')->paginate(12);
--- resources/views/livewire/admin/admin-order-component.blade.php
+++ resources/views/livewire/admin/admin-order-component.blade.php
@@ -1,3 +1,23 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        All Orders
+                    </div>
+                    <div class="panel-body">
+                        <table class="table-striped">
+                            <thead>
+                                <tr>
+                                    <th>OrderId</th>
+                                    <th>OrderId</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
