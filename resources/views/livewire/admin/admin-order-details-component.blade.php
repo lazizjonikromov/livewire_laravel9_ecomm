ccdc565b6d4bb51838fe6350bc523cbad39ce06e
Laziz <lazizjonweb@gmail.com>
2022-06-01 16:18:52 +0500

    work

--- resources/views/livewire/admin/admin-order-details-component.blade.php
+++ resources/views/livewire/admin/admin-order-details-component.blade.php
@@ -15,44 +15,34 @@
                                     <li class="pr-cart-item">
                                         <div class="product-image">
                                             <figure><img
-                                                    src="{{ asset('assets/images/products') }}/{{ $item->model->image }}"
-                                                    alt="{{ $item->model->name }}"></figure>
+                                                    src="{{ asset('assets/images/products') }}/{{ $item->product->image }}"
+                                                    alt="{{ $item->product->name }}"></figure>
                                         </div>
                                         <div class="product-name">
                                             <a class="link-to-product"
-                                                href="{{ route('product.details', ['slug' => $item->model->slug]) }}">{{ $item->model->name }}</a>
+                                                href="{{ route('product.details', ['slug' => $item->product->slug]) }}">{{ $item->product->name }}</a>
                                         </div>
                                         <div class="price-field produtc-price">
-                                            <p class="price">${{ $item->model->regular_price }}</p>
+                                            <p class="price">${{ $item->price }}</p>
                                         </div>
                                         <div class="quantity">
-                                            <div class="quantity-input">
-                                                <input type="text" name="product-quatity" value="{{ $item->qty }}"
-                                                    data-max="120" pattern="[0-9]*">
-                                                <a class="btn btn-increase" href="#"
-                                                    wire:click.prevent="increaseQuantity('{{ $item->rowId }}')"></a>
-                                                <a class="btn btn-reduce" href="#"
-                                                    wire:click.prevent="decreaseQuantity('{{ $item->rowId }}')"></a>
-                                            </div>
-                                            <p class="text-center">
-                                                <a href="#"
-                                                    wire:click.prevent="switchToSaveForLater('{{ $item->rowId }}')">Save
-                                                    For Later</a>
-                                            </p>
+                                            <h5>{{ $item->quantity }}</h5>
                                         </div>
                                         <div class="price-field sub-total">
-                                            <p class="price">${{ $item->subtotal }}</p>
-                                        </div>
-                                        <div class="delete">
-                                            <a href="#" wire:click.prevent="destroy('{{ $item->rowId }}')"
-                                                class="btn btn-delete" title="">
-                                                <span>Delete from your cart</span>
-                                                <i class="fa fa-times-circle" aria-hidden="true"></i>
-                                            </a>
+                                            <p class="price">${{ $item->price * $item->quantity }}</p>
                                         </div>
                                     </li>
                                 @endforeach
                             </ul>
+                        </div>
+                        <div class="summary">
+                            <div class="order-summary">
+                                <h4 class="title-box">Order Summary</h4>
+                                <p class="summary-info"><span class="title">Subtotal</span><b class="index">${{ $order->subtotal }}</b></p>
+                                <p class="summary-info"><span class="title">Subtotal</span><b class="index">${{ $order->subtotal }}</b></p>
+                                <p class="summary-info"><span class="title">Subtotal</span><b class="index">${{ $order->subtotal }}</b></p>
+                                <p class="summary-info"><span class="title">Subtotal</span><b class="index">${{ $order->subtotal }}</b></p>
+                            </div>
                         </div>
                     </div>
                 </div>
