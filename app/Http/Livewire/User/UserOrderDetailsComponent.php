8c840322616a3f5b8bc5d916d73ac407f9a22989
Laziz <lazizjonweb@gmail.com>
2022-06-02 15:18:01 +0500

    work

--- app/Http/Livewire/User/UserOrderDetailsComponent.php
+++ app/Http/Livewire/User/UserOrderDetailsComponent.php
@@ -5,15 +5,19 @@ namespace App\Http\Livewire\User;
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
-use Livewire\WithPagination;
 
 class UserOrderDetailsComponent extends Component
 {
-    use WithPagination;
+    public $order_id;
+
+    public function mount($order_id)
+    {
+        $this->order_id = $order_id;
+    }
 
     public function render()
     {
-        $orders = Order::where('user_id',Auth::user()->id)->paginate(12);
-        return view('livewire.user.user-order-details-component', ['orders'=>$orders])->layout('layouts.base');
+        $order = Order::where('user_id',Auth::user()->id)->where('id', $this->order_id)->first();
+        return view('livewire.user.user-order-details-component', ['order'=>$order])->layout('layouts.base');
     }
 }
--- app/Http/Livewire/User/UserOrdersComponent.php
+++ app/Http/Livewire/User/UserOrdersComponent.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UserOrdersComponent extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.user.user-orders-component')->layout('layouts.base');
+        $orders = Order::where('user_id',Auth::user()->id)->paginate(12);
+        return view('livewire.user.user-orders-component', ['orders'=>$orders])->layout('layouts.base');
     }
 }
