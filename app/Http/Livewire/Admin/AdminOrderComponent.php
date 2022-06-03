995f90ba452716f28f3f75872dad5f7394c216a8
Laziz <lazizjonweb@gmail.com>
2022-06-03 17:01:28 +0500

    work

--- app/Http/Livewire/Admin/AdminOrderComponent.php
+++ app/Http/Livewire/Admin/AdminOrderComponent.php
@@ -10,6 +10,16 @@ class AdminOrderComponent extends Component
 {
     use WithPagination;
 
+    public function updateOrderStatus($order_id,$status)
+    {
+        $order = Order::find($order_id);
+        $order->status = $status;
+        if($status == 'delivered')
+        {
+            
+        }
+    }
+
     public function render()
     {
         $orders = Order::orderBy('created_at','DESC')->paginate(12);
