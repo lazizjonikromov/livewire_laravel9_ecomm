96adca72f52d9ee8a63eb36c8f965258c7919cb8
Laziz <lazizjonweb@gmail.com>
2022-06-04 16:30:46 +0500

    work

--- resources/views/livewire/user/user-change-password-component.blade.php
+++ resources/views/livewire/user/user-change-password-component.blade.php
@@ -1,3 +1,20 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <div class="container" style="padding: 30px 0;">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Change Password
+                    </div>
+                    <div class="panel-body">
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Current Password</label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
