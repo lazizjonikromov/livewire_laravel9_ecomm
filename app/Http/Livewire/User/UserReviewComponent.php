6e74d192a451f903a7f8312fec5844de3efcab98
Laziz <lazizjonweb@gmail.com>
2022-06-04 11:12:41 +0500

    work

---
+++ app/Http/Livewire/User/UserReviewComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class UserReviewComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.user-review-component');
+    }
+}
