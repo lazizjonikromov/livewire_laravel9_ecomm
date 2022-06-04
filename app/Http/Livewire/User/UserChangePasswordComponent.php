c03d7d9d26f06dd2b7f0e1767c7201bb68dcc77d
Laziz <lazizjonweb@gmail.com>
2022-06-04 16:50:48 +0500

    work

--- app/Http/Livewire/User/UserChangePasswordComponent.php
+++ app/Http/Livewire/User/UserChangePasswordComponent.php
@@ -10,9 +10,20 @@ class UserChangePasswordComponent extends Component
     public $password;
     public $password_confirmation;
 
+    public function updated($fields)
+    {
+        $this->validateOnly($fields , [
+            'current_password'=>'required',
+            'password'=>'required|min:8|confirmed|different:current_password'
+        ]);
+    }
+
     public function changePassword()
     {
-        
+        $this->validate([
+            'current_password'=>'required',
+            'password'=>'required|min:8|confirmed|different:current_password'
+        ]);
     }
 
     public function render()
