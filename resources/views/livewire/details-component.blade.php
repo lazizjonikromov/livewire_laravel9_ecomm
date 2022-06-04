37fe1be2228e96539877536dd64d14724de97377
Laziz <lazizjonweb@gmail.com>
2022-06-04 11:39:01 +0500

    work

--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -110,31 +110,9 @@
 
 									<div class="wrap-review-form">
 
-										<div id="comments">
-											<h2 class="woocommerce-Reviews-title">01 review for <span>Radiant-360 R6 Chainsaw Omnidirectional [Orage]</span></h2>
-											<ol class="commentlist">
-												<li class="comment byuser comment-author-admin bypostauthor even thread-even depth-1" id="li-comment-20">
-													<div id="comment-20" class="comment_container">
-														<img alt="" src="{{ asset('assets/images/author-avata.jpg') }}" height="80" width="80">
-														<div class="comment-text">
-															<div class="star-rating">
-																<span class="width-80-percent">Rated <strong class="rating">5</strong> out of 5</span>
-															</div>
-															<p class="meta">
-																<strong class="woocommerce-review__author">admin</strong>
-																<span class="woocommerce-review__dash">–</span>
-																<time class="woocommerce-review__published-date" datetime="2008-02-14 20:00" >Tue, Aug 15,  2017</time>
-															</p>
-															<div class="description">
-																<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-															</div>
-														</div>
-													</div>
-												</li>
-											</ol>
-										</div><!-- #comments -->
-
 										
+
+
 
 									</div>
 								</div>
