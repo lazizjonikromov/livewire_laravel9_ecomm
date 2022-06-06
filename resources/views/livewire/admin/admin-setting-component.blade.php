0827d720cdf2e13f98f07cec158acd067d43e2d7
Laziz <lazizjonweb@gmail.com>
2022-06-06 14:22:00 +0500

    work

--- resources/views/livewire/admin/admin-setting-component.blade.php
+++ resources/views/livewire/admin/admin-setting-component.blade.php
@@ -12,70 +12,70 @@
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Email</label>
                             <div class="col-md-4">
-                                <input type="email" placeholder="Email" class="form-control input-md">
+                                <input type="email" placeholder="Email" class="form-control input-md" wire:model="email">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Phone</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Phone" class="form-control input-md">
+                                <input type="text" placeholder="Phone" class="form-control input-md" wire:model="phone">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Phone2</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Phone2" class="form-control input-md">
+                                <input type="text" placeholder="Phone2" class="form-control input-md" wire:model="phone2">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Address</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Address" class="form-control input-md">
+                                <input type="text" placeholder="Address" class="form-control input-md" wire:model="address">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Map</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Map" class="form-control input-md">
+                                <input type="text" placeholder="Map" class="form-control input-md" wire:model="map">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Twiter</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Twiter" class="form-control input-md">
+                                <input type="text" placeholder="Twiter" class="form-control input-md" wire:model="twiter">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Facebook</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Facebook" class="form-control input-md">
+                                <input type="text" placeholder="Facebook" class="form-control input-md" wire:model="facebook">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Pinterest</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Pinterest" class="form-control input-md">
+                                <input type="text" placeholder="Pinterest" class="form-control input-md" wire:model="pinterest">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Instagram</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Instagram" class="form-control input-md">
+                                <input type="text" placeholder="Instagram" class="form-control input-md" wire:model="instagram">
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label for="" class="col-md-4 control-label">Youtube</label>
                             <div class="col-md-4">
-                                <input type="text" placeholder="Youtube" class="form-control input-md">
+                                <input type="text" placeholder="Youtube" class="form-control input-md" wire:model="youtube">
                             </div>
                         </div>
 
