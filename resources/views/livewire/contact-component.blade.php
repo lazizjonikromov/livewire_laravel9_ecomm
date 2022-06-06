a1d482e671bf29ff811816b563135d23ca2a4cf8
Laziz <lazizjonweb@gmail.com>
2022-06-06 16:30:47 +0500

    work

--- resources/views/livewire/contact-component.blade.php
+++ resources/views/livewire/contact-component.blade.php
@@ -80,7 +80,7 @@
                                     <i class="fa fa-envelope" aria-hidden="true"></i>
                                     <div class="right-info">
                                         <b>Email</b>
-                                        <p>Support1@Mercado.com</p>
+                                        <p>{{ $setting->email }}</p>
                                     </div>
                                 </div>
 
@@ -88,15 +88,15 @@
                                     <i class="fa fa-phone" aria-hidden="true"></i>
                                     <div class="right-info">
                                         <b>Phone</b>
-                                        <p>0123-465-789-111</p>
+                                        <p>{{ $setting->phone }}</p>
                                     </div>
                                 </div>
 
                                 <div class="icon-box-item">
                                     <i class="fa fa-map-marker" aria-hidden="true"></i>
                                     <div class="right-info">
-                                        <b>Mail Office</b>
-                                        <p>Sed ut perspiciatis unde omnis<br />Street Name, Los Angeles</p>
+                                        <b>Address</b>
+                                        <p>{{ $setting->address }}</p>
                                     </div>
                                 </div>
 
