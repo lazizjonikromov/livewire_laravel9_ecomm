6e91faaed1465c03bd77b547fbbdcf85f991cd64
Laziz <lazizjonweb@gmail.com>
2022-06-07 15:10:58 +0500

    work

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,7 +47,7 @@
     </x-jet-authentication-card>
 </x-guest-layout> --}}
 
-<x-guest-layout>
+<x-base-layout>
     	<!--main area-->
 	<main id="main" class="main-site left-sidebar">
 
@@ -62,13 +62,13 @@
 			<div class="row">
 				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">
 					<div class=" main-content-area">
-						<div class="wrap-login-item ">						
+						<div class="wrap-login-item ">
 							<div class="login-form form-item form-stl">
                                 <x-jet-validation-errors class="mb-4" />
 								<form name="frm-login" method="POST" action="{{ route('login') }}">
                                     @csrf
 									<fieldset class="wrap-title">
-										<h3 class="form-title">Log in to your account</h3>										
+										<h3 class="form-title">Log in to your account</h3>
 									</fieldset>
 									<fieldset class="wrap-input">
 										<label for="frm-login-uname">Email Address:</label>
@@ -78,7 +78,7 @@
 										<label for="frm-login-pass">Password:</label>
 										<input type="password" id="frm-login-pass" name="password" placeholder="************" required autocomplete="current-password">
 									</fieldset>
-									
+
 									<fieldset class="wrap-input">
 										<label class="remember-field">
 											<input class="frm-input " name="remember" id="rememberme" value="forever" type="checkbox"><span>Remember me</span>
@@ -87,9 +87,9 @@
 									</fieldset>
 									<input type="submit" class="btn btn-submit" value="Login" name="submit">
 								</form>
-							</div>												
+							</div>
 						</div>
-					</div><!--end main products area-->		
+					</div><!--end main products area-->
 				</div>
 			</div><!--end row-->
 
@@ -97,4 +97,4 @@
 
 	</main>
 	<!--main area-->
-</x-guest-layout>
+</x-base-layout>
