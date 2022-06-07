31eab3786b2ab410e10bdd5d9861b68aefcc00b5
Laziz <lazizjonweb@gmail.com>
2022-06-07 14:45:23 +0500

    work

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -59,7 +59,7 @@
                 <div class="row">
                   <div class="col-xs-8 text-left">
                     <span class="icon-stat-label">Total Revenue</span>
-                    <span class="icon-stat-value">$0</span>
+                    <span class="icon-stat-value">${{ $totalRevenue }}</span>
                   </div>
                   <div class="col-xs-4 text-center">
                     <i class="fa fa-dollar icon-stat-visual bg-primary"></i>
@@ -75,7 +75,7 @@
                 <div class="row">
                   <div class="col-xs-8 text-left">
                     <span class="icon-stat-label">Total Sales</span>
-                    <span class="icon-stat-value">0</span>
+                    <span class="icon-stat-value">{{ $totalSales }}</span>
                   </div>
                   <div class="col-xs-4 text-center">
                     <i class="fa fa-gift icon-stat-visual bg-secondary"></i>
@@ -91,7 +91,7 @@
                 <div class="row">
                   <div class="col-xs-8 text-left">
                     <span class="icon-stat-label">Today Revenue</span>
-                    <span class="icon-stat-value">$0</span>
+                    <span class="icon-stat-value">${{ $todayRevenue }}</span>
                   </div>
                   <div class="col-xs-4 text-center">
                     <i class="fa fa-dollar icon-stat-visual bg-primary"></i>
@@ -107,7 +107,7 @@
                 <div class="row">
                   <div class="col-xs-8 text-left">
                     <span class="icon-stat-label">Today Sales</span>
-                    <span class="icon-stat-value">0</span>
+                    <span class="icon-stat-value">{{ $todaySales }}</span>
                   </div>
                   <div class="col-xs-4 text-center">
                     <i class="fa fa-gift icon-stat-visual bg-secondary"></i>
@@ -118,6 +118,57 @@
                 </div>
               </div>
             </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12">
+                  <div class="panel panel-default">
+                      <div class="panel-heading">
+                          Latest Order
+                      </div>
+                      <div class="panel-body">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Order Id</th>
+                                    <th>Subtotal</th>
+                                    <th>Discount</th>
+                                    <th>Tax</th>
+                                    <th>Total</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Mobile</th>
+                                    <th>Email</th>
+                                    <th>Zipcode</th>
+                                    <th>Status</th>
+                                    <th>Order Date</th>
+                                    <th class="text-center">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($orders as $order)
+                                    <tr>
+                                        <td>{{ $order->id }}</td>
+                                        <td>${{ $order->subtotal }}</td>
+                                        <td>${{ $order->discount }}</td>
+                                        <td>${{ $order->tax }}</td>
+                                        <td>${{ $order->total }}</td>
+                                        <td>{{ $order->firstname }}</td>
+                                        <td>{{ $order->lastname }}</td>
+                                        <td>{{ $order->mobile }}</td>
+                                        <td>{{ $order->email }}</td>
+                                        <td>{{ $order->zipcode }}</td>
+                                        <td>{{ $order->status }}</td>
+                                        <td>{{ $order->created_at }}</td>
+                                        <td>
+                                            <a href="{{ route('admin.orderdetails',['order_id'=>$order->id]) }}" class="btn btn-info btn-sm">Details</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                      </div>
+                  </div>
+              </div>
           </div>
     </div>
 </div>
