04d9f0428709b6b03afaa485f2f65b7e57b88504
Laziz <lazizjonweb@gmail.com>
2022-06-18 10:57:19 +0500

    work

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -163,9 +163,8 @@
                     <div class="widget-content">
                         <ul class="list-category">
                             @foreach ($categories as $category)
-                                <li class="category-item">
-                                    <a href="{{ route('product.category', ['category_slug' => $category->slug]) }}"
-                                        class="cate-link">{{ $category->name }}</a>
+                                <li class="category-item {{count($category->subCategories) > 0 ? 'has-child-cate': '' }}">
+                                    <a href="{{ route('product.category', ['category_slug' => $category->slug]) }}" class="cate-link">{{ $category->name }}</a>
                                 </li>
                             @endforeach
                         </ul>
