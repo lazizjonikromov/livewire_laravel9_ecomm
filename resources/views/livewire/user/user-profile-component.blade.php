b6257200aec969f918bcb89d9af63c9f20f5cc2b
Laziz <lazizjonweb@gmail.com>
2022-06-21 15:54:55 +0500

    work

--- resources/views/livewire/user/user-profile-component.blade.php
+++ resources/views/livewire/user/user-profile-component.blade.php
@@ -8,11 +8,22 @@
                 <div class="panel-body">
                     <div class="col-md-4">
                         @if ($user->profile->image)
-                            <img src="{{  }}" alt="">
+                            <img src="{{ asset('assets/images/profile') }}/{{ $user->profile->image }}" alt="" width="100%">
+                        @else
+                            <img src="{{ asset('assets/images/profile/default.jpg') }}" alt="" width="100%">
                         @endif
                     </div>
                     <div class="col-md-8">
-
+                        <p><b>Name: </b>{{ $user->name }}</p>
+                        <p><b>Email: </b>{{ $user->email }}</p>
+                        <p><b>Phone: </b>{{ $user->profile->mobile }}</p>
+                        <hr>
+                        <p><b>Line1: </b>{{ $user->profile->line1 }}</p>
+                        <p><b>Line2: </b>{{ $user->profile->line2 }}</p>
+                        <p><b>City: </b>{{ $user->profile->city }}</p>
+                        <p><b>Province: </b>{{ $user->profile->province }}</p>
+                        <p><b>Country: </b>{{ $user->profile->country }}</p>
+                        <p><b>Zip Code: </b>{{ $user->profile->zipcode }}</p>
                     </div>
                 </div>
             </div>
