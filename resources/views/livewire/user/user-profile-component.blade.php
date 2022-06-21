40098e63fbbe80b8894341e71f6a5eadf12935af
Laziz <lazizjonweb@gmail.com>
2022-06-21 15:45:28 +0500

    work

--- resources/views/livewire/user/user-profile-component.blade.php
+++ resources/views/livewire/user/user-profile-component.blade.php
@@ -1,3 +1,21 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <div class="container" style="padding: 30px 0">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Profile
+                </div>
+                <div class="panel-body">
+                    <div class="col-md-4">
+                        @if ($user->profile->image)
+                            <img src="{{  }}" alt="">
+                        @endif
+                    </div>
+                    <div class="col-md-8">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
