a55c6b6d700952ac3f6f8475d9b6937da1f33230
Laziz <lazizjonweb@gmail.com>
2022-06-21 16:02:46 +0500

    work

--- app/Http/Livewire/User/UserProfileComponent.php
+++ app/Http/Livewire/User/UserProfileComponent.php
@@ -16,6 +16,7 @@ class UserProfileComponent extends Component
         {
             $profile = new Profile();
             $profile->user_id = Auth::user()->id;
+            $profile->save();
         }
         $user = User::find(Auth::user()->id);
         return view('livewire.user.user-profile-component', ['user'=>$user])->layout('layouts.base');
