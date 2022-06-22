6e0b60c78263e45dcc36330e72287e26625b10ae
Laziz <lazizjonweb@gmail.com>
2022-06-22 14:50:32 +0500

    the end

--- resources/views/livewire/user/user-edit-profile-component.blade.php
+++ resources/views/livewire/user/user-edit-profile-component.blade.php
@@ -6,26 +6,28 @@
                     Update Profile
                 </div>
                 <div class="panel-body">
-                    <div class="col-md-4">
-                        @if ($user->profile->image)
-                            <img src="{{ asset('assets/images/profile') }}/{{ $user->profile->image }}" alt="" width="100%">
-                        @else
-                            <img src="{{ asset('assets/images/profile/default.jpg') }}" alt="" width="100%">
-                        @endif
-                        <input type="file" class="form-control">
-                    </div>
-                    <div class="col-md-8">
-                        <p><b>Name: </b>{{ $user->name }}</p>
-                        <p><b>Email: </b>{{ $user->email }}</p>
-                        <p><b>Phone: </b>{{ $user->profile->mobile }}</p>
-                        <hr>
-                        <p><b>Line1: </b>{{ $user->profile->line1 }}</p>
-                        <p><b>Line2: </b>{{ $user->profile->line2 }}</p>
-                        <p><b>City: </b>{{ $user->profile->city }}</p>
-                        <p><b>Province: </b>{{ $user->profile->province }}</p>
-                        <p><b>Country: </b>{{ $user->profile->country }}</p>
-                        <p><b>Zip Code: </b>{{ $user->profile->zipcode }}</p>
-                    </div>
+                    <form>
+                        <div class="col-md-4">
+                            @if ($user->profile->image)
+                                <img src="{{ asset('assets/images/profile') }}/{{ $user->profile->image }}" alt="" width="100%">
+                            @else
+                                <img src="{{ asset('assets/images/profile/default.jpg') }}" alt="" width="100%">
+                            @endif
+                            <input type="file" class="form-control">
+                        </div>
+                        <div class="col-md-8">
+                            <p><b>Name: </b><input type="text" class="form-control"></p>
+                            <p><b>Email: </b>{{ $user->email }}</p>
+                            <p><b>Phone: </b><input type="text" class="form-control"></p>
+                            <hr>
+                            <p><b>Line1: </b><input type="text" class="form-control"></p>
+                            <p><b>Line2: </b><input type="text" class="form-control"></p>
+                            <p><b>City: </b><input type="text" class="form-control"></p>
+                            <p><b>Province: </b><input type="text" class="form-control"></p>
+                            <p><b>Country: </b><input type="text" class="form-control"></p>
+                            <p><b>Zip Code: </b><input type="text" class="form-control"></p>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
