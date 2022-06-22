9106ef86fe25204e83e9d200c45df8383478d922
Laziz <lazizjonweb@gmail.com>
2022-06-22 14:53:10 +0500

    work

--- app/Http/Livewire/User/UserEditProfileComponent.php
+++ app/Http/Livewire/User/UserEditProfileComponent.php
@@ -6,6 +6,17 @@ use Livewire\Component;
 
 class UserEditProfileComponent extends Component
 {
+    public $name;
+    public $email;
+    public $mobile;
+    public $image;
+    public $line1;
+    public $line2;
+    public $city;
+    public $province;
+    public $country;
+    public $zipcode;
+
     public function render()
     {
         return view('livewire.user.user-edit-profile-component')->layout('layouts.base');
