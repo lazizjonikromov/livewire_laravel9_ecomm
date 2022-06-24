da47bbd716a9023b3a81ec6c06cf231c7608acc0
Laziz <lazizjonweb@gmail.com>
2022-06-24 11:23:04 +0500

    work

---
+++ app/Http/Livewire/Admin/AdminAttributesComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminAttributesComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-attributes-component');
+    }
+}
