1ecabfa6ea0d936b401bb501cb8073c7ba61a712
Laziz <lazizjonweb@gmail.com>
2022-06-24 12:25:44 +0500

    work

--- app/Http/Livewire/Admin/AdminEditAttributeComponent.php
+++ app/Http/Livewire/Admin/AdminEditAttributeComponent.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\ProductAttribute;
 use Livewire\Component;
 
 class AdminEditAttributeComponent extends Component
 {
+    public $name;
+    public $attribute_id;
+
+    public function mount($attribute_id)
+    {
+        $pattribute = ProductAttribute::find($attribute_id);
+        
+
+    }
+
     public function render()
     {
         return view('livewire.admin.admin-edit-attribute-component')->layout('layouts.base');
